0aaa7f03def2f8900171455cd8e84a2d80ad086a
Keith Njagi Gatana <kgatana@safaricom.co.ke>
2023-08-18 11:26:14 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+'''
+Main file
+'''
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    '''
+    This is the home route
+    '''
     return "<h1>Hello from USIU</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5005)
